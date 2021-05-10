0831855f655aa85b0e8fe6805e2fc6cb0e696d2c
Dornavineeth <vineethdorna@gmail.com>
2021-05-10 18:45:10 +0530

    adding in orders table

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const Cart = require('../models/cart');
 const User = require("../models/user");
 // const Prod = require('../models/prod');
@@ -5,20 +6,33 @@ const User = require("../models/user");
 
 exports.get_cart = async (req,res,next) => {
 
-    const cartitem = new Cart();
-    const email = req.oidc.user.email;
-    const usr = new User(email);
-    const details_id = await usr.getDetailsId().catch(err=>console.log(err));
+    if(req.oidc.isAuthenticated()){
+        const cartitem = new Cart();
+        const email = req.oidc.user.email;
+        const usr = new User(email);
+        const details_id = await usr.getDetailsId().catch(err=>console.log(err));
 
-    const cartitems = await cartitem.get_all(details_id);
-    // console.log(cartitems.rows);
-    // console.log(req.details_id)
-    res.render('includes/cart.ejs', {
-        pageTitle: 'Cart',
-        path: '/cart',
-        editing: false,
-        items: cartitems.rows
-    });
+        const cartitems = await cartitem.get_all(details_id);
+        const addritems = await usr.getAddressesUsingDetailsId(details_id);
+        // console.log(cartitems.rows);
+        // console.log(req.details_id);
+        // console.log(addritems);
+        res.render('includes/cart.ejs', {
+            pageTitle: 'Cart',
+            path: '/cart',
+            editing: false,
+            items: cartitems.rows,
+            addrs: addritems.rows
+        });
+    }
+    else{
+        //unauthenticated  home page
+        res.send(JSON.stringify({
+            pageTitle:'Home',
+            alertMessage:false,
+            debugString : 'In get_home,unauthenticated'
+        }));
+    }
 };
 
 // exports.post_cart = async (req,res,next) => {
---
+++ src/models/transactions.js
@@ -0,0 +1,20 @@
+const pool= require('../utils/database');
+
+module.exports = class Transactions{
+    
+    constructor(){
+        this.trans_id = null;        
+    }
+
+    async add_transaction(cartvalue,status='pending',description=''){
+        var now = new Date();
+        var date = now.getFullYear()+'-'+now.getMonth()+'-'+now.getDate()
+        var time = now.getHours() +':'+now.getMinutes()+':00'+'+05:30'
+        var start_time = date+' '+time
+        var trans_id =  await pool.query('INSERT INTO transactions(transaction_description,start_time,trans_status,cost) VALUES ($1,$2,$3,$4) RETURNING transaction_id',
+                        [description,start_time,status,cartvalue]);
+        this.trans_id = trans_id.rows[0].transaction_id;
+        return this.trans_id;        
+    }
+
+}
