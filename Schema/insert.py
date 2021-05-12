ff89ae22b39f7fe29d547185ced34fe723883e75
rishyanthkondra <rishyanthkondra@gmail.com>
2021-05-13 00:58:05 +0530

    added tabless.sql

--- Schema/insert.py
+++ Schema/insert.py
@@ -1,18 +1,18 @@
 import csv
 
-def insert(insert_file,data_file,columns):
+def insert(insert_file,data_file,columns,name):
 	csvfile = open(data_file,'r')
 	output = open(insert_file,'w+')
 	reader = csv.reader(csvfile)
 	for row in reader:
 		myrow = ["'"+row[i]+"'" for i in range(len(columns))]
-		output.write(f"INSERT INTO DISH({','.join(columns)}) VALUES({','.join(myrow)});\n")
+		output.write(f"INSERT INTO {name}({','.join(columns)}) VALUES({','.join(myrow)});\n")
 
 dish_columns = ['dish_name','cost_per_unit','image_url','dish_description','nutritional_info','health_info','cusine','dish_type','sub_type']
-insert("insertScripts/dish.sql","data/dish.csv",dish_columns)
+insert("insertScripts/dish.sql","data/dish.csv",dish_columns,'dish')
 
 ingredient_columns = ['ingredient_name','ingredient_description','image_url','unit','cost_per_unit']
-insert("insertScripts/ingredients.sql","data/ingredients.csv",ingredient_columns)
+insert("insertScripts/ingredients.sql","data/ingredients.csv",ingredient_columns,'ingredients')
 
 
 
